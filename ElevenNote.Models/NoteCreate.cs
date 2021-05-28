61c6df9f94cb574d7d93360abb9a4f78013b3106
Taylor <taylorfulner@gmail.com>
2021-05-28 12:40:43 -0400

    models added to Models tier

--- ElevenNote.Models/NoteCreate.cs
+++ ElevenNote.Models/NoteCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,11 @@ namespace ElevenNote.Models
 {
     class NoteCreate
     {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
     }
 }
